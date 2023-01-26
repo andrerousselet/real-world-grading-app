3b87790fa417beaa2129c0156d84bb76e260a1e8
Andre Rousselet <andre098@hotmail.com>
2023-01-26 15:16:23 -0300

    feat(seed.ts): create new course and respective tests (lika a transaction)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,49 @@
 import { PrismaClient } from '@prisma/client'
+import { add } from 'date-fns'
 
 const prisma = new PrismaClient()
 
 async function main() {
-  const user = await prisma.user.create({
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: "johndoe@email.com",
+  //     firstName: "John",
+  //     lastName: "Doe",
+  //     social: {
+  //       facebook: "johndoe",
+  //       twitter: "johndoe",
+  //     }
+  //   }
+  // })
+
+  await prisma.test.deleteMany()
+  await prisma.course.deleteMany()
+
+  const weekFromNow = add(new Date(), { days: 7 })
+  const twoWeeksFromNow = add(new Date(), { days: 14 })
+  const monthFromNow = add(new Date(), { days: 30 })
+
+  const course = await prisma.course.create({
     data: {
-      email: "johndoe@email.com",
-      firstName: "John",
-      lastName: "Doe",
-      social: {
-        facebook: "johndoe",
-        twitter: "johndoe",
+      name: "CRUD with Prisma in the real world",
+      courseDetails: "A soft introduction to CRUD with Prisma",
+      tests: {
+        createMany: {
+          data: [
+            { date: weekFromNow, name: "First test" },
+            { date: twoWeeksFromNow, name: "Second test" },
+            { date: monthFromNow, name: "Final exam" },
+          ]
+        }
       }
+    },
+    include: {
+      tests: true
     }
   })
+
+  console.log(course);
+
 }
 
 main()
