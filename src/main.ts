05226310864ee47b93a9d51db10526a32808ce71
Andre Rousselet <andre098@hotmail.com>
2023-02-02 09:41:51 -0300

    feat(aggregation queries): find aggregates for specific student

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,29 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const tests = await prisma.test.findMany()
-  for (const test of tests) {
-    const results = await prisma.testResult.aggregate({
-      where: {
-        testId: test?.id,
-      },
-      _avg: { result: true },
-      _max: { result: true },
-      _min: { result: true },
-      _count: true,
-    })
-    console.log(`test: ${test.name} (id: ${test.id})`, results)
-  }
+  // const tests = await prisma.test.findMany()
+  // for (const test of tests) {
+  //   const results = await prisma.testResult.aggregate({
+  //     where: {
+  //       testId: test?.id,
+  //     },
+  //     _avg: { result: true },
+  //     _max: { result: true },
+  //     _min: { result: true },
+  //     _count: true,
+  //   })
+  //   console.log(`test: ${test.name} (id: ${test.id})`, results)
+  // }
+  const davidAggregates = await prisma.testResult.aggregate({
+    where: {
+      student: { email: "david@prisma.io" },
+    },
+    _avg: { result: true },
+    _max: { result: true },
+    _min: { result: true },
+    _count: true,
+  })
+  console.log(`David's results`, davidAggregates)
 }
 
 
